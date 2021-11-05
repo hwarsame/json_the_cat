ae438c3c8a329f9afc303e312831ace0c7ca312c
Hamza Warsame <hamza360123@gmail.com>
2021-11-05 00:41:15 -0400

    final refacror

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,25 +1,17 @@
 const request = require('request');
-const fs = require('fs');
 
-let userInput = process.argv[2];
-
-const data = function(breedName) {
-  console.log(breedName);
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${userInput}`, (error , response, body)=> {
-    // console.log (body);
-    if (error === true) {
-      console.log('Error:', error);
+const fetchBreedDescription = function(breedName, callback) {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    const data = JSON.parse(body);
+    if (data[0]) {
+      callback(null, data[0].description );
     } else {
-      // console.log('body:', body.length)
-      const bodyB = JSON.parse(body);
-      // console.log('bodyB:', bodyB.length)
-      if (bodyB.length === 0) {
-        console.log('Breed was not found');
-      } else {
-        console.log(bodyB[0].description);
-      }
+      callback(null, null)
     }
   });
 };
-
-data(userInput);
+module.exports = { fetchBreedDescription };
